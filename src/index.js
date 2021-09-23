a1d7550f899441ea727ca9d7e8b193233751f1d4
Yana Nechveeva <79226190+Yana1112pstr@users.noreply.github.com>
2021-09-23 10:43:04 +0300

    update function timer

--- src/index.js
+++ src/index.js
@@ -18,38 +18,31 @@ class CountdownTimer {
     let currentDate = Date.now()
     let deltaTime = this.targetDate.getTime() - currentDate
 
+    
+    function getTimeComponents(time) {
+        let days = Math.floor(time / (1000 * 60 * 60 * 24));
+        days = String(days).padStart(2, "0");
+        let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        hours = String(hours).padStart(2, "0");
+        let mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+        mins = String(mins).padStart(2, "0");
+        let secs = Math.floor((time % (1000 * 60)) / 1000);
+        secs = String(secs).padStart(2, "0");
+    
+        return {days, hours, mins, secs};
+    };
+
     const time = getTimeComponents(deltaTime)
     const {days, hours, mins, secs} = time;
-  
-    insertData(this.refs.fieldDays, days);
-    insertData(this.refs.fieldHours, hours);
-    insertData(this.refs.fieldMins, mins);
-    insertData(this.refs.fieldSecs, secs)
-    //this.refs.fieldDays.textContent = days;
-    //this.refs.fieldHours.textContent = hours;
-    //this.refs.fieldMins.textContent = mins;
-    //this.refs.fieldSecs.textContent = secs;
+
+    this.refs.fieldDays.textContent = days;
+    this.refs.fieldHours.textContent = hours;
+    this.refs.fieldMins.textContent = mins;
+    this.refs.fieldSecs.textContent = secs;
 }, 1000)
 }
 
 }
-
-function insertData(place, value) {
-    place.textContent = value
-}
-
-function getTimeComponents(time) {
-    let days = Math.floor(time / (1000 * 60 * 60 * 24));
-    days = String(days).padStart(3, "0");
-    let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    hours = String(hours).padStart(2, "0");
-    let mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    mins = String(mins).padStart(2, "0");
-    let secs = Math.floor((time % (1000 * 60)) / 1000);
-    secs = String(secs).padStart(2, "0");
-
-    return {days, hours, mins, secs};
-};
 
 
 const timer1 = new CountdownTimer({
